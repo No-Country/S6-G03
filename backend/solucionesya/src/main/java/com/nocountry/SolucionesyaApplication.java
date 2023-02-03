e17fc1c5ff3f8e7f852a64c035d93d7203396137
AstroCaro <silvagarcesc@gmail.com>
2023-02-02 22:11:23 -0300

    First commit

---
+++ backend/solucionesya/src/main/java/com/nocountry/SolucionesyaApplication.java
@@ -0,0 +1,13 @@
+package com.nocountry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolucionesyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolucionesyaApplication.class, args);
+	}
+
+}
