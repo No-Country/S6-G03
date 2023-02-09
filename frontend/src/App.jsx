c301a09c8a46afe0b72e1a86cdfe1d84b2dd6848
rafa2701 <leonr2701@gmail.com>
2023-02-09 00:02:53 -0400

    instanciar rutas por defecto frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,52 @@
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar"
 
 const App = () => {
   return (
     <>
-      <Navbar/>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+    <Route index element={<h1>Inicio</h1>} />
+    {/* Rutas de Login */}
+    <Route path="login-usuario" element={<h1>Login de Usuario</h1>} />
+    <Route path="login-profesional" element={<h1>Login de Profesional</h1>} />
+    {/* Rutas de Registro */}
+    <Route path="registro-usuario" element={<h1>Registro de Profesional</h1>} />
+    <Route path="registro-profesional" element={<h1>Registro de Profesional</h1>} />
+    {/* Rutas de Busqueda de profesional */}
+    <Route path="categorias-profesionales" element={<h1>Categorias de Profesionales</h1>} />
+    <Route path="profesionales/:categoria" element={<h1>Lista de Profesionales</h1>} />
+    <Route path="detalle-profesional/:id" element={<h1>Detalle de un solo Profesional</h1>} />
+    {/* Rutas por defecto */}
+    <Route path="restringido" element={<h1>No Autorizado</h1>} />
+    
+    </Routes>
+    {/* Rutas del Panel de Usuario */}
+    <Route
+            path="perfil-usuario"
+            element={/*user ? <UserPanel /> : <Navigate to="/login" replace />*/ <h1>Perfil de usuario</h1>}
+          >
+            <Route index element={<h1>Perfil usuario</h1>} />
+            <Route path="editar" element={<h1>Editar perfil</h1>} />
+            <Route path="contratos" element={<h1>Contratos</h1>} />
+            <Route path="change-password" element={<h3>Cambiar pass</h3>} />
+            <Route path="mensajes" element={<h3>Mis Mensajes</h3>} />
+          </Route>
+    {/* Rutas del Panel de Profesional */}
+    <Route
+            path="perfil-profesional"
+            element={/*user ? <UserPanel /> : <Navigate to="/login" replace />*/ <h1>Perfil de profesional</h1>}
+          >
+            <Route index element={<h1>Perfil profesional</h1>} />
+            <Route path="editar" element={<h1>Editar perfil</h1>} />
+            <Route path="contratos" element={<h1>Contratos</h1>} />
+            <Route path="change-password" element={<h3>Cambiar pass</h3>} />
+            <Route path="mensajes" element={<h3>Mis Mensajes</h3>} />
+          </Route>
+      {/* Aqui puede ir el footer */}
+    </BrowserRouter>
+     
     </>
   )
 }
