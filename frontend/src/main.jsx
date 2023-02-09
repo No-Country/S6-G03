4f4d3d10a206343adfdfed19742efd272739b1a1
Diego Juarez <diegoalbertojuarez@yahoo.com>
2023-02-09 16:57:15 -0600

    Revert "Beto"

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,15 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { store } from './Redux/store'
-import { Provider } from 'react-redux'
-import Navbar from './Components/Navbar/Navbar'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
   <React.StrictMode>
-    <Navbar />
     <App />
-  </React.StrictMode>
-  </Provider>
+  </React.StrictMode>,
 )
