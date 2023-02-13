c447e655bf628c31b8bb5bd44cb2e7f6c7372859
rafa2701 <leonr2701@gmail.com>
2023-02-13 18:01:04 -0400

    cambios register

--- frontend/src/components/Register/UserRegister.jsx
+++ frontend/src/components/Register/UserRegister.jsx
@@ -3,14 +3,16 @@ import {Formik, Form, Field, ErrorMessage } from 'formik';
 import {useState, useEffect} from 'react'
 
 const userRegister = () => {
-  // const [setOauth, oAuth]=useState(false)
+  // const [setOauth, oAuth]=useState(false);
   return (
     <>
     <div className="container mx-auto my-4 p-2 text-center">
-    <h1>Mi perfil</h1>
+    <h1 className="text-2xl py-5">Mi perfil</h1>
       <Formik
       initialValues={{
-
+        user:'',
+        password:'',
+        image:null,
         firstName: '',
         lastName: '',
         email:'',
@@ -44,6 +46,33 @@ const userRegister = () => {
      >
        {({ isSubmitting }) => (
          <Form className="mx-auto max-w-sm">
+          
+           
+           <div>
+            {/* <label className="block text-sm font-medium text-gray-700">Photo</label> */}
+              <div className="mt-1 flex items-center"> 
+                <button
+                  type="button"
+                  className="ml-5 rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  >
+                    <svg className="h-full w-full text-gray-300 " fill="currentColor" viewBox="0 0 24 24">
+                        <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                    </svg>
+                     
+                </button>
+              </div>
+              <label className="flex-initial">Cargar Imagen</label>
+           <ErrorMessage name="user" component="div" />
+                  </div>
+
+
+          <label className="flex-initial">Usuario</label>
+           <Field type="text" name="user"  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" />
+           <ErrorMessage name="user" component="div" />
+           <label className="flex-initial">Contraseña</label>
+           <Field type="password" name="password" className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name"/>
+           <ErrorMessage name="password" component="div" />
+          <hr className="my-4"></hr>
           <label className="flex-initial">Nombre</label>
            <Field className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" name="firstName" />
            <ErrorMessage name="name" component="div" />
@@ -59,11 +88,9 @@ const userRegister = () => {
            <label className="flex-initial">Address</label>
            <Field type="text" name="phone" className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name"/>
            <ErrorMessage name="phone" component="div" />
-           <label className="flex-initial">Contraseña</label>
-           <Field type="password" name="password" className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name"/>
-           <ErrorMessage name="password" component="div" />
-           <button className="mx-auto my-2 flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" type="submit" disabled={isSubmitting}>
-             Enviar
+          
+           <button className="mx-auto my-5 flex justify-center  border border-transparent bg-gray-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" type="submit" disabled={isSubmitting}>
+             Guardar
            </button>
          </Form>
        )}
