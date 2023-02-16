ae4e747fc250b7088dc5752caeab5fc4f5f4bd36
rafa2701 <leonr2701@gmail.com>
2023-02-16 01:13:48 -0400

    card setup

--- frontend/src/components/Category/SingleProfessional.jsx
+++ frontend/src/components/Category/SingleProfessional.jsx
@@ -2,21 +2,69 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import "./SingleProfessional.css";
+import {professionals} from "./dummyDB.js";
+
 
 const SingleProfessional = () => {
   const { id } = useParams();
-  const [oneProfessional, setOneProfessional] = useState("");
-
+  const [professional, setProfessional] = useState(professionals[0]);
+  let reviewsCount = 0;
   useEffect(() => {
-    const url = `${import.meta.env.VITE_BACKEND_URL}/api/profesional/${id}`;
+//     const url = `${import.meta.env.VITE_BACKEND_URL}/api/profesional/${id}`;
 
-    axios
-      .get(url)
-      .then((res) => setOneProduct(res.data))
-      .catch((err) => console.log(err));
+//     axios
+//       .get(url)
+//       .then((res) => setOneProduct(res.data))
+//       .catch((err) => console.log(err));
+ //setProfessional(professionals[0]);
+
+
+
   }, []);
+  console.log(professional.reviews.length);
 
-  return <div>SingleProfessional</div>;
+  return (
+  <>
+    <div></div>
+    <div className="flex professional-card mx-auto my-5 grid-cols-2 text-white" >
+    <div className="col-1 flex-auto flex-col  place-content-evenly">
+        <div className="pf-img rounded"></div>
+        <div className="my-"></div>
+        <h5 className="card-title">{professional.firstName} {professional.lastName}</h5>
+        <p className="card-text">(rating)({professional.reviews.length?professional.reviews.length:0})</p>
+        <a href="#" className="btn btn-primary">Ver Reseñas</a>
+    </div>
+    <div className="col-2">
+        <div className="text-white">{professional.description}</div>
+        <div>Otros servicios que presta:</div>
+        <h3 className="">Valor Presupuesto: {professional.budget}</h3>
+        <div class="flex space-x-2 justify-center">
+        <button
+            type="button"
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+            class="inline-block w-full px-6 py-4 bg-green-secondary text-white font-bold text-lg leading-tight uppercase rounded shadow-md hover:bg-green-secondary/60 hover:shadow-lg focus:bg-green-secondary/60 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+        >Contratar</button>
+        </div>
+        <p>La Contratacion Incluye: Visita al domicilio y presupuesto del servicio</p>
+    </div>
+    </div>
+
+    <div className="flex justify-center">
+  <div className="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
+    <img className=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg" src="https://mdbootstrap.com/wp-content/uploads/2020/06/vertical.jpg" alt />
+    <div className="p-6 flex flex-col justify-start">
+      <h5 className="text-gray-900 text-xl font-medium mb-2">Card title</h5>
+      <p className="text-gray-700 text-base mb-4">
+        This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+      </p>
+      <p className="text-gray-600 text-xs">Last updated 3 mins ago</p>
+    </div>
+  </div>
+</div>
+
+  </>);
 };
 
 export default SingleProfessional;
---
+++ frontend/src/components/Category/dummyDB.js
@@ -0,0 +1,39 @@
+export const professionals= [
+        {
+            firstName:"Alberto",
+            lastName:"Paez",
+            skills:["Plomeria", "Gasista"],
+            profileImg:"image url",
+            description:"!Hola Comunidad! Soy beto, soy plomero y gasista matriculado... lorem ipsum dolor",
+            budget:1000,
+            zone:"Lanús",
+            reviews:[{
+                "title":"Excelente trabajo",
+                "rating":5
+            },
+            {
+                "title":"buen trabajo 2",
+                "rating":4
+            },
+            {
+                "title":"regular trabajo ",
+                "rating":3
+            }],
+            jobs:[{
+                "title":"Reemplazo de Cañeria",
+                "date":"29/12/2022",
+                "zone":"Gerli",
+                "client":"Susana Villagraña",
+                "review":"Estoy confirme con el servicio que me brindo beto. ademas dejo todo muy limpio",
+                "reviewID":"001"
+            },
+            {
+                "title":"Instalación de inodoro",
+                "date":"03/01/2023",
+                "zone":"Lanús",
+                "client":"Stella Peris",
+                "review":"Estoy conforme con el servicio que me brindo beto. ademas dejo todo muy limpio",
+                "reviewID":"002"
+            }]
+        },
+    ]
