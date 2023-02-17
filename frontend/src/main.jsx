598749cdf1ddaf1e66ac41b7aaaed4d532d569c4
Juarez-Diego <diegoalbertojuarez@yahoo.com>
2023-02-17 16:50:45 -0600

    Changed to lowercase for components

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { store } from './Redux/store'
+import { store } from './Redux/store.js'
 import { Provider } from 'react-redux'
-import Navbar from './Components/Navbar/Navbar'
+import Navbar from './components/Navbar/Navbar'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
