2f35e1fa973fb1c0ee2aaf7de7aa59a61257925d
rafa2701 <leonr2701@gmail.com>
2023-02-19 21:54:25 -0400

    reviews-count font size increase

--- frontend/src/components/Category/StarRating.jsx
+++ frontend/src/components/Category/StarRating.jsx
@@ -50,7 +50,7 @@ const StarRating = ({rating, reviews}) => {
         <path fill={`url(#grad-${p})`} d="M20.388,10.918L32,12.118l-8.735,7.749L25.914,31.4l-9.893-6.088L6.127,31.4l2.695-11.533L0,12.118
       l11.547-1.2L16.026,0.6L20.388,10.918z"/>
       </svg>)
-    })} {miniRating?<span className='text-xs'>({rating})</span>:<span className='text-xs'>({reviews})</span>}
+    })} {miniRating?<span className='text-[16px]'>({rating})</span>:<span className='text-[16px]'>({reviews})</span>}
     </div>
     
     </>
