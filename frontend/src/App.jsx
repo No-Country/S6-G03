f8a29ff42146a6195554b4fe5af9028ddc7b2861
rafa2701 <leonr2701@gmail.com>
2023-02-21 13:36:17 -0400

    fixing commit errors

---
+++ frontend/src/App.jsx
@@ -0,0 +1,62 @@
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
+import UserProfile from "./components/UserProfile/UserProfile";
+import Direcciones from "./components/Addresses/Addresses";
+import Navbar from './components/Navbar/Navbar';
+
+import 'flowbite';
+
+const App = () => {
+  return (
+    <>
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+    <Route index element={<Home />} />
+    {/* Rutas de Login */}
+    <Route path="login-usuario" element={<h1>Login de Usuario</h1>} />
+    <Route path="login-profesional" element={<h1>Login de Profesional</h1>} />
+    {/* Rutas de Registro */}
+    <Route path="registro-usuario" element={<h1>Registro de Profesional</h1>} />
+    <Route path="registro-profesional" element={<h1>Registro de Profesional</h1>} />
+    {/* Rutas de Busqueda de profesional */}
+    <Route path="categorias-profesionales" element={<h1>Categorias de Profesionales</h1>} />
+    <Route path="profesionales/:categoria" element={<h1>Lista de Profesionales</h1>} />
+    <Route path="detalle-profesional/:id" element={<h1>Detalle de un solo Profesional</h1>} />
+    {/* Rutas por defecto */}
+    <Route path="restringido" element={<h1>No Autorizado</h1>} />
+    
+    
+    {/* Rutas del Panel de Usuario */}
+    <Route index element={<UserProfile />} />
+          <Route path="perfil-usuario" element={/*user ? <UserPanel /> : <Navigate to="/login" replace />*/ <UserProfile />}>
+  
+            <Route path="editar" element={<h1>Editar perfil</h1>} />
+            <Route path="contratos" element={<h1>Contratos</h1>} />
+            <Route path="change-password" element={<h3>Cambiar pass</h3>} />
+            <Route path="mensajes" element={<h3>Mis Mensajes</h3>} />
+          </Route>
+          <Route path="perfil-usuario/editar-direcciones" element={<Direcciones />} />
+  
+    {/* Rutas del Panel de Profesional */}
+    <Route
+            path="perfil-profesional"
+            element={/*user ? <UserPanel /> : <Navigate to="/login" replace />*/ <h1>Perfil de profesional</h1>}
+          >
+            <Route index element={<h1>Perfil profesional</h1>} />
+            <Route path="editar" element={<h1>Editar perfil</h1>} />
+            <Route path="contratos" element={<h1>Contratos</h1>} />
+            <Route path="change-password" element={<h3>Cambiar pass</h3>} />
+            <Route path="mensajes" element={<h3>Mis Mensajes</h3>} />
+          </Route>
+    </Routes>               
+      {/* Aqui puede ir el footer */}
+      <Footer />
+    </BrowserRouter>
+     
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Home/Home.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import SearchBar from "../SearchBar/Searchbar";
+
+function Home(){
+    return(
+        <div className="p-8"> 
+            
+            <div className="flex flex-col justify-center items-center text-center">
+                <div className="p-8"> 
+                    <div>
+                        <span className="font-[700] text-[45px] leading-64">¿Necesitas un profesional urgente? <br/>Conseguilo al instante con SolucionesYa!</span></div>
+                    </div>
+
+                <div className="p-8">
+                    <div>
+                        <span className="font-[400] text-[45px] leading-56">¿Qué tipo de profesional buscas?</span>
+                    </div>
+
+                    <SearchBar />
+                </div>
+
+                <div className="flex flex-col justify-around items-center bg-[#1E9E69] w-[1120px] h-[360px] p-3 rounded-[40px] p-8">
+                    <div>
+                        <span className="font-[700] text-[45px] leading-64 text-[#FFFFFF]">Ventajas de usar SolucionesYa!</span>
+                    </div>
+
+                    <div className="flex flex-row justify-around items-center w-full" >
+                        <div className="flex flex-col justify-around items-center bg-[#1E9E69] w-[280px] h-[200px] border-4 border-[#9CD5BE] rounded-[40px]">
+                            <img src="../../../src/assets/images/Location.png" alt="location" />
+                            <span className="font-[400] text-[23px] leading-28 text-[#FFFFFF] text-center">Podes contactar al profesional más cercano</span>
+                        </div>
+                        <div className="flex flex-col justify-around items-center bg-[#1E9E69] w-[280px] h-[200px] border-4 border-[#9CD5BE] rounded-[40px]">
+                            <img src="../../../src/assets/images/S.O.S..png" alt="location" />
+                            <span className="font-[400] text-[23px] leading-28 text-[#FFFFFF] text-center">Podes encontrar servicios las 24hs del dia</span>
+                        </div>
+                        <div className="flex flex-col justify-around items-center bg-[#1E9E69] w-[280px] h-[200px] border-4 border-[#9CD5BE] rounded-[40px]">
+                            <div className="flex flex-row">
+                                <img src="../../../src/assets/images/Star.png" alt="location" />
+                                <img src="../../../src/assets/images/Star.png" alt="location" />
+                                <img src="../../../src/assets/images/Star.png" alt="location" />
+                                <img src="../../../src/assets/images/Star.png" alt="location" />
+                                <img src="../../../src/assets/images/Star.png" alt="location" />
+                            </div>
+                            <span className="font-[400] text-[23px] leading-28 text-[#FFFFFF] text-center">Podes mirar las opiniones de clientes anteriores</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex flex-col items-center justify-around p-8 w-full h-[500px]">
+                    <div><span className="font-[400] text-[43px] leading-56">Servicios más buscados</span></div>
+                    <div className="flex flex-row items-center justify-around w-full">
+                        <div>
+                            <img src="../../../src/assets/images/Plomeria.png" alt="" className="w-[261px] h-[250px] rounded-[40px]"/>
+                            <span className="font-[400] text-[24px] leading-40">Plomeria</span>
+                        </div>
+                     
+                        <div>
+                            <img src="../../../src/assets/images/Cerrajeria.png" alt="" className="w-[261px] h-[250px] rounded-[40px]"/>
+                            <span className="font-[400] text-[24px] leading-40">Cerrajeria</span>
+                        </div>
+          
+                        <div>
+                            <img src="../../../src/assets/images/Electricista.png" alt="" className="w-[261px] h-[250px] rounded-[40px]"/>
+                            <span className="font-[400] text-[24px] leading-40">Electricidad</span>
+                        </div>
+
+                        <div>
+                            <img src="../../../src/assets/images/GAsista.png" alt="" className="w-[261px] h-[250px] rounded-[40px]"/>
+                            <span className="font-[400] text-[24px] leading-40">Gasista</span>
+                        </div>
+                    </div>
+
+                    <div className="my-[15px]">
+                        <button className="bg-[#1E9E69] w-[504px] h-[68px] rounded-[12px] content-center font-[700] text-[32px] leading-36 text-[#FFFFFF]">Ver todos los servicios</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
