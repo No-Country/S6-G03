edcb5acccb475a9926120904f1c4b681b0f8934b
Hernán Federico Costarelli <hernancostarelli@gmail.com>
2023-02-27 20:46:15 -0300

    fix( Contract ): Modification in application.properties for deploy
    Reviewed-by: Hernán Federico Costarelli <hernancostarelli@gmail.com>

--- backend/solucionesya/src/main/java/com/nocountry/service/impl/CloudinaryServiceImpl.java
+++ backend/solucionesya/src/main/java/com/nocountry/service/impl/CloudinaryServiceImpl.java
@@ -32,7 +32,7 @@ public class CloudinaryServiceImpl implements ICloudinaryService {
 //
 //        System.setProperty("dotenv.path", absolutePath );
 
-        System.setProperty("dotenv.path", "/backend/.env");
+        System.setProperty("dotenv.path", "/backend/solucionesya/.env");
         Dotenv dotenv = Dotenv.load();
         cloudinary = new Cloudinary(dotenv.get("CLOUDINARY_URL"));
         cloudinary.config.secure = true;
