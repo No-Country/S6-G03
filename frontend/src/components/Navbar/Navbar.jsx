6585d69ba2334e0359a5960a9263e54c08ccdf3c
pernajulian <julianperna@gmail.com>
2023-02-28 15:37:25 -0300

    register and login fixed

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -9,78 +9,77 @@ const Navbar = () => {
   const [auth, setAuth] = useState(false);
 
   useEffect(() => {
-
-  setAuth(localStorage.getItem("auth"));
-  setUser(localStorage.getItem("user"));
-  
-
+    setAuth(localStorage.getItem("auth"));
+    setUser(localStorage.getItem("user"));
   }, [localStorage.getItem("user")]);
-  
+
   //  const handleClick = () => {
-      
+
   //   };
-  
-    const handleLogin = () => {
-     if(auth){
-      console.log("Cerrando sesion... "+auth);
+
+  const handleLogin = () => {
+    if (auth) {
+      console.log("Cerrando sesion... " + auth);
       localStorage.removeItem("user");
       localStorage.removeItem("pwd");
       localStorage.setItem("auth", false);
       setUser("");
       setAuth(false);
-     } 
+    }
     //  else{
     //   navigate("/login-usuario") ;
     //   setUser("");
     //   localStorage.setItem("auth", false);
-    //  } 
-      
-    };
-
-    
+    //  }
+  };
 
   return (
     <>
       <nav className="px-5 navbar justify-between flex   bg-blue-primary py-3 items-center text-white  ">
-        <Link to="/"><div className=" flex justify-start w-4/5  md:mx-auto">
-          <img  src="/brand-logo.svg" alt="Logo" className="brand-logo  " />
-        </div></Link>
-        
+        <Link to="/">
+          <div className=" flex justify-start w-4/5  md:mx-auto">
+            <img src="/brand-logo.svg" alt="Logo" className="brand-logo  " />
+          </div>
+        </Link>
+
         {/* <a href="" className="flex md:mr-60">
           Inicio
         </a> */}
-        
+
         <div className=" flex justify-end">
           <div className="flex items-center mt-4 font-[400] text-[18px] leading-28">
             <a href="" className="md:mr-30 sm:mr-10">
               Quienes Somos
             </a>
-
           </div>
-          {user?<div className="text-center mt-4 px-5"> {user}</div>:""}
-         {user? 
-        //  Elementos si esta logueado
-          
-         <div className="flex  gap-1  flex-col">
-          
-            <button onClick={handleLogin} className="w-[140px]  h-[40px] font-[400] text-[18px] leading-28  mt-3 border border-red-800 rounded-md bg-red-800">
-              Cerrar Sesión
-            </button>
-            
-          </div>:
-          //Elementos si no hay user logueado 
-         <div className="flex  gap-1  flex-col">
-          <Link to="/login-usuario">
-          <button className="w-[140px]  h-[40px] font-[400] text-[18px] leading-28  border border-green-secondary rounded-md">
-              Iniciar Sesion
-            </button>
-          </Link>
-            
-            <Link to="/registro-usuario"><button className="w-[140px]  h-[40px] font-[400] text-[18px] leading-28 border border-green-secondary rounded-md bg-green-secondary">
-              Registrarme
-            </button></Link>
-            
-          </div>}
+          {user ? <div className="text-center mt-4 px-5"> {user}</div> : ""}
+          {user ? (
+            //  Elementos si esta logueado
+
+            <div className="flex  gap-1  flex-col">
+              <button
+                onClick={handleLogin}
+                className="w-[140px]  h-[40px] font-[400] text-[18px] leading-28  mt-3 border border-red-800 rounded-md bg-red-800"
+              >
+                Cerrar Sesión
+              </button>
+            </div>
+          ) : (
+            //Elementos si no hay user logueado
+            <div className="flex  gap-1  flex-col">
+              <Link to="/login-usuario">
+                <button className="w-[140px]  h-[40px] font-[400] text-[18px] leading-28  border border-green-secondary rounded-md">
+                  Iniciar Sesion
+                </button>
+              </Link>
+
+              <Link to="/registro-usuario">
+                <button className="w-[140px]  h-[40px] font-[400] text-[18px] leading-28 border border-green-secondary rounded-md bg-green-secondary">
+                  Registrarme
+                </button>
+              </Link>
+            </div>
+          )}
         </div>
       </nav>
     </>
