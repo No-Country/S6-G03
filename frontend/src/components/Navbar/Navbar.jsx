fec12baf35a436e15ed7519e921a3ef65f7c3ab9
Luciano-Olmedo <lucianonicolasolmedo@gmail.com>
2023-02-28 22:12:33 -0300

    adding link to perfil and minor fixes in navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { Navigate, useNavigate, Link } from "react-router-dom";
 import "./Navbar.css";
+import muñeco from "../../assets/images/pf-image.jpg"
+import { FaChevronDown } from "react-icons/fa";
+
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -10,77 +13,93 @@ const Navbar = () => {
 
   useEffect(() => {
 
-  setAuth(localStorage.getItem("auth"));
-  setUser(localStorage.getItem("user"));
-  
+    setAuth(localStorage.getItem("auth"));
+    setUser(localStorage.getItem("user"));
+
 
   }, [localStorage.getItem("user")]);
-  
+
   //  const handleClick = () => {
-      
+
   //   };
-  
-    const handleLogin = () => {
-     if(auth){
-      console.log("Cerrando sesion... "+auth);
+
+  const handleLogin = () => {
+    if (auth) {
+      console.log("Cerrando sesion... " + auth);
       localStorage.removeItem("user");
       localStorage.removeItem("pwd");
       localStorage.setItem("auth", false);
       setUser("");
       setAuth(false);
-     } 
+    }
     //  else{
     //   navigate("/login-usuario") ;
     //   setUser("");
     //   localStorage.setItem("auth", false);
     //  } 
-      
-    };
 
-    
+  };
+
+
 
   return (
     <>
       <nav className="px-5 navbar justify-between flex   bg-blue-primary py-3 items-center text-white  ">
         <Link to="/"><div className=" flex justify-start w-4/5  md:mx-auto">
-          <img  src="/brand-logo.svg" alt="Logo" className="brand-logo  " />
+          <img src="/brand-logo.svg" alt="Logo" className="brand-logo  " />
         </div></Link>
-        
+
         {/* <a href="" className="flex md:mr-60">
           Inicio
         </a> */}
-        
+
         <div className=" flex justify-end">
           <div className="flex items-center mt-4 font-[400] text-[18px] leading-28">
-            <a href="" className="md:mr-30 sm:mr-10">
-              Quienes Somos
-            </a>
+            {
+              user ?
+                ""
+                :
+                <a href="" className="md:mr-30 sm:mr-10">
+                  Quienes Somos
+                </a>
+            }
 
           </div>
-          {user?<div className="text-center mt-4 px-5"> {user}</div>:""}
-         {user? 
-        //  Elementos si esta logueado
-          
-         <div className="flex  gap-1  flex-col">
-          
-            <button onClick={handleLogin} className="w-[140px]  h-[40px] font-[400] text-[18px] leading-28  mt-3 border border-red-800 rounded-md bg-red-800">
-              Cerrar Sesión
-            </button>
-            
-          </div>:
-          //Elementos si no hay user logueado 
-         <div className="flex  gap-1  flex-col">
-          <Link to="/login-usuario">
-          <button className="w-[140px]  h-[40px] font-[400] text-[18px] leading-28  border border-green-secondary rounded-md">
-              Iniciar Sesion
-            </button>
-          </Link>
-            
-            <Link to="/registro-usuario"><button className="w-[140px]  h-[40px] font-[400] text-[18px] leading-28 border border-green-secondary rounded-md bg-green-secondary">
-              Registrarme
-            </button></Link>
-            
-          </div>}
+
+          {user ?
+            //  Elementos si esta logueado
+
+            <div className="flex flex-col items-center  ">
+              <div className="flex-row flex justify-between  gap-4  ">
+                <div className="flex">
+                  <Link to="/perfil-usuario">
+                    <img src={muñeco} alt="muñeco" className="w-8 h-8 border rounded-full translate-y-3 " />
+                  </Link>
+
+                </div>
+                <div className="text-center mt-4 text-[20px]"> {user}</div>
+
+
+
+              </div>
+              <button onClick={handleLogin} className="w-[140px]  h-[40px] font-[400] text-[18px] leading-28  mt-3 border border-red-800 rounded-md bg-red-800">
+                Cerrar Sesión
+              </button>
+
+            </div> :
+            //Elementos si no hay user logueado 
+            <div className="flex  gap-1  flex-col">
+              <Link to="/login-usuario">
+                <button className="w-[140px]  h-[40px] font-[400] text-[18px] leading-28  border border-green-secondary rounded-md">
+                  Iniciar Sesion
+                </button>
+              </Link>
+
+              <Link to="/registro-usuario"><button className="w-[140px]  h-[40px] font-[400] text-[18px] leading-28 border border-green-secondary rounded-md bg-green-secondary">
+                Registrarme
+              </button></Link>
+
+            </div>}
         </div>
       </nav>
     </>
